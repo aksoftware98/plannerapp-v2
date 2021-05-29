45d995ca990b5700499099e1439af301d88444c6
Ahmad <aksoftware98@hotmail.com>
2021-05-29 15:28:06 +0300

    Create the auth layout & the login form

---
+++ PlannerApp.Shared/Models/LoginRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Shared.Models
+{
+    public class LoginRequest
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(6)]
+        public string Password { get; set; }
+
+    }
+}
---
+++ src/PlannerApp/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlannerApp.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Components
+{
+    public partial class LoginForm : ComponentBase
+    {
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        private LoginRequest _model = new LoginRequest(); 
+
+        private async Task LoginUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
